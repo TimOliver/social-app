b96c5b3b6e2e52099a1efed73755712a95fc3894
Tim Oliver <git@tim.dev>
2026-05-03 23:05:13 +0900

    Add gradient to overflowing Trending pills on Explore page

--- src/components/interstitials/Trending.tsx
+++ src/components/interstitials/Trending.tsx
@@ -1,8 +1,10 @@
 import {useCallback} from 'react'
 import {ScrollView, View} from 'react-native'
+import {LinearGradient} from 'expo-linear-gradient'
 import {msg} from '@lingui/core/macro'
 import {useLingui} from '@lingui/react'
 
+import {useReadableContentInsets} from '#/lib/hooks/useReadableContentInsets'
 import {
   useTrendingSettings,
   useTrendingSettingsApi,
@@ -12,6 +14,7 @@ import {useTrendingConfig} from '#/state/service-config'
 import {LoadingPlaceholder} from '#/view/com/util/LoadingPlaceholder'
 import {BlockDrawerGesture} from '#/view/shell/BlockDrawerGesture'
 import {atoms as a, useGutters, useTheme} from '#/alf'
+import {transparentifyColor} from '#/alf/util/colorGeneration'
 import {Button, ButtonIcon} from '#/components/Button'
 import {EdgeToEdgeBleed} from '#/components/EdgeToEdgeBleed'
 import {TimesLarge_Stroke2_Corner0_Rounded as X} from '#/components/icons/Times'
@@ -36,6 +39,10 @@ export function Inner() {
   const {setTrendingDisabled} = useTrendingSettingsApi()
   const {data: trending, error, isLoading} = useTrendingTopics()
   const noTopics = !isLoading && !error && !trending?.topics?.length
+  const insets = useReadableContentInsets()
+  const hasInset = insets.left > 0 || insets.right > 0
+  const bgOpaque = t.atoms.bg.backgroundColor
+  const bgTransparent = transparentifyColor(bgOpaque, 0)
 
   const onConfirmHide = useCallback(() => {
     ax.metric('trendingTopics:hide', {context: 'interstitial'})
@@ -45,88 +52,125 @@ export function Inner() {
   return error || noTopics ? null : (
     <>
       <EdgeToEdgeBleed />
-      <BlockDrawerGesture>
-        <ScrollView
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          decelerationRate="fast">
-          <View style={[gutters, a.flex_row, a.align_center, a.gap_lg]}>
-            <View style={{paddingLeft: 4, paddingRight: 2}}>
-              <Graph size="sm" />
-            </View>
-            {isLoading ? (
-              <View style={[a.py_lg, a.flex_row, a.gap_lg, a.align_center]}>
-                <LoadingPlaceholder
-                  width={80}
-                  height={undefined}
-                  style={{alignSelf: 'stretch'}}
-                />
-                <LoadingPlaceholder
-                  width={50}
-                  height={undefined}
-                  style={{alignSelf: 'stretch'}}
-                />
-                <LoadingPlaceholder
-                  width={120}
-                  height={undefined}
-                  style={{alignSelf: 'stretch'}}
-                />
-                <LoadingPlaceholder
-                  width={30}
-                  height={undefined}
-                  style={{alignSelf: 'stretch'}}
-                />
-                <LoadingPlaceholder
-                  width={180}
-                  height={undefined}
-                  style={{alignSelf: 'stretch'}}
-                />
-                <Text
-                  style={[
-                    t.atoms.text_contrast_medium,
-                    a.text_sm,
-                    a.font_semi_bold,
-                  ]}>
-                  {' '}
-                </Text>
+      <View
+        style={
+          hasInset && {marginLeft: -insets.left, marginRight: -insets.right}
+        }>
+        <BlockDrawerGesture>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            decelerationRate="fast"
+            contentContainerStyle={
+              hasInset && {
+                paddingLeft: insets.left,
+                paddingRight: insets.right,
+              }
+            }>
+            <View style={[gutters, a.flex_row, a.align_center, a.gap_lg]}>
+              <View style={{paddingLeft: 4, paddingRight: 2}}>
+                <Graph size="sm" />
               </View>
-            ) : !trending?.topics ? null : (
-              <>
-                {trending.topics.map(topic => (
-                  <TrendingTopicLink
-                    key={topic.link}
-                    topic={topic}
-                    onPress={() => {
-                      ax.metric('trendingTopic:click', {
-                        context: 'interstitial',
-                      })
-                    }}>
-                    <View style={[a.py_lg]}>
-                      <Text
-                        style={[
-                          t.atoms.text_contrast_medium,
-                          a.text_sm,
-                          a.font_semi_bold,
-                        ]}>
-                        {topic.topic}
-                      </Text>
-                    </View>
-                  </TrendingTopicLink>
-                ))}
-                <Button
-                  label={_(msg`Hide trending topics`)}
-                  size="tiny"
-                  variant="ghost"
-                  color="secondary"
-                  shape="round"
-                  onPress={() => trendingPrompt.open()}>
-                  <ButtonIcon icon={X} />
-                </Button>
-              </>
-            )}
-          </View>
-        </ScrollView>
-      </BlockDrawerGesture>
+              {isLoading ? (
+                <View style={[a.py_lg, a.flex_row, a.gap_lg, a.align_center]}>
+                  <LoadingPlaceholder
+                    width={80}
+                    height={undefined}
+                    style={{alignSelf: 'stretch'}}
+                  />
+                  <LoadingPlaceholder
+                    width={50}
+                    height={undefined}
+                    style={{alignSelf: 'stretch'}}
+                  />
+                  <LoadingPlaceholder
+                    width={120}
+                    height={undefined}
+                    style={{alignSelf: 'stretch'}}
+                  />
+                  <LoadingPlaceholder
+                    width={30}
+                    height={undefined}
+                    style={{alignSelf: 'stretch'}}
+                  />
+                  <LoadingPlaceholder
+                    width={180}
+                    height={undefined}
+                    style={{alignSelf: 'stretch'}}
+                  />
+                  <Text
+                    style={[
+                      t.atoms.text_contrast_medium,
+                      a.text_sm,
+                      a.font_semi_bold,
+                    ]}>
+                    {' '}
+                  </Text>
+                </View>
+              ) : !trending?.topics ? null : (
+                <>
+                  {trending.topics.map(topic => (
+                    <TrendingTopicLink
+                      key={topic.link}
+                      topic={topic}
+                      onPress={() => {
+                        ax.metric('trendingTopic:click', {
+                          context: 'interstitial',
+                        })
+                      }}>
+                      <View style={[a.py_lg]}>
+                        <Text
+                          style={[
+                            t.atoms.text_contrast_medium,
+                            a.text_sm,
+                            a.font_semi_bold,
+                          ]}>
+                          {topic.topic}
+                        </Text>
+                      </View>
+                    </TrendingTopicLink>
+                  ))}
+                  <Button
+                    label={_(msg`Hide trending topics`)}
+                    size="tiny"
+                    variant="ghost"
+                    color="secondary"
+                    shape="round"
+                    onPress={() => trendingPrompt.open()}>
+                    <ButtonIcon icon={X} />
+                  </Button>
+                </>
+              )}
+            </View>
+          </ScrollView>
+        </BlockDrawerGesture>
+        {hasInset && (
+          <>
+            <LinearGradient
+              pointerEvents="none"
+              colors={[bgOpaque, bgOpaque, bgTransparent]}
+              locations={[0, 2 / 3, 1]}
+              start={{x: 0, y: 0.5}}
+              end={{x: 1, y: 0.5}}
+              style={[
+                a.absolute,
+                {top: 0, bottom: 0, left: 0, width: insets.left},
+              ]}
+            />
+            <LinearGradient
+              pointerEvents="none"
+              colors={[bgTransparent, bgOpaque, bgOpaque]}
+              locations={[0, 1 / 3, 1]}
+              start={{x: 0, y: 0.5}}
+              end={{x: 1, y: 0.5}}
+              style={[
+                a.absolute,
+                {top: 0, bottom: 0, right: 0, width: insets.right},
+              ]}
+            />
+          </>
+        )}
+      </View>
       <EdgeToEdgeBleed />
 
       <Prompt.Basic
